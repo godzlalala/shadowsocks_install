d85510e2caf5406a00527946918ce4a4eb317c31
zots0127 <zots0127@gmail.com>
2017-07-27 21:23:17 +0800

    Update shadowsocksR.sh

--- shadowsocksR.sh
+++ shadowsocksR.sh
@@ -314,7 +314,7 @@ pre_install(){
 # Download files
 download_files(){
     # Download libsodium file
-    if ! wget --no-check-certificate -O libsodium-1.0.13.tar.gz https://github.com/zots0127/libsodium/releases/download/1.0.13/libsodium-1.0.13.tar.gz; then
+    if ! wget --no-check-certificate -O libsodium-1.0.13.tar.gz https://github.com/jedisct1/libsodium/releases/download/1.0.13/libsodium-1.0.13.tar.gz; then
         echo -e "[${red}Error${plain}] Failed to download libsodium-1.0.13.tar.gz!"
         exit 1
     fi
